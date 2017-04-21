6a2abdb1e4b9892c62a8a0e4f75ab252a69d40a2
swamp09 <shu.numata@esm.co.jp>
2017-04-21 17:20:22 +0900

    add About page
    modify static_pages spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,10 +26,12 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+# logger
+#Rails.logger = Logger.new(STDOUT) # 追記
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  config.include Capybara::DSL
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
