1ef10735d941658df60d859b22920f02e430c53e
swamp09 <shu.numata@esm.co.jp>
2017-04-27 17:45:46 +0900

    Implement advanced login

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,16 +18,19 @@ RSpec.describe User, type: :model do
 
     it { expect(@user).to_not be_valid }
   end
+
   describe 'when name is not present' do
     before { @user.email = ' ' }
 
     it { expect(@user).to_not be_valid }
   end
+
   describe 'name be too long' do
     before { @user.name = 'a' * 51 }
 
     it { expect(@user).to_not be_valid }
   end
+
   describe 'email be too long' do
     before { @user.email = 'a' * 244 + '@example.com' }
 
@@ -38,12 +41,14 @@ RSpec.describe User, type: :model do
     it 'should be valid' do
       invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                              foo@bar_baz.com foo@bar+baz.com]
+
       invalid_addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).to_not be_valid, "#{invalid_address.inspect} is not correct"
       end
     end
   end
+
   describe 'email addresses should be unique' do
     it 'unique' do
       duplicate_user = @user.dup
@@ -51,6 +56,7 @@ RSpec.describe User, type: :model do
 
       expect(duplicate_user).to_not be_valid
     end
+
     it 'address saved as lower-case' do
       mixed_case_email = 'Foo@ExAMPle.CoM'
       @user.email = mixed_case_email
@@ -59,17 +65,24 @@ RSpec.describe User, type: :model do
       expect(mixed_case_email.downcase).to eq(@user.reload.email)
     end
   end
+
   describe 'password' do
     it 'password should be present (nonblank)' do
-
       @user.password = @user.password_confirmation = ' ' * 6
 
       expect(@user).to_not be_valid
     end
+
     it 'password should have a minimum length' do
       @user.password = @user.password_confirmation = 'a' * 5
 
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe 'authenticated?' do
+    it 'user with nil digest' do
+      expect @user.authenticated?('')
     end
   end
 end
--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "UsersLogins", type: :request do
+RSpec.describe 'UsersLogins', type: :request do
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: {session: {email: user.email,
+                                        password: password,
+                                        remember_me: remember_me}}
+  end
+
   before do
     @user = FactoryGirl.create(:michael)
   end
 
-  describe "GET /users_logins" do
-    it "with invalid infomation" do
+  describe 'GET /users_logins' do
+    it 'with invalid infomation' do
       visit login_path
 
-      click_button "Log in"
+      click_button 'Log in'
 
       expect(page).to have_title('Log in | Ruby on Rails Tutorial Sample App')
 
@@ -21,16 +27,16 @@ RSpec.describe "UsersLogins", type: :request do
     end
   end
 
-  describe "login valid infomation" do
+  describe 'login valid infomation' do
     before do
       visit login_path
 
       fill_in 'Email', with: @user.email
       fill_in 'Password', with: @user.password
 
-      click_button "Log in"
+      click_button 'Log in'
     end
-    it "infomation valid" do
+    it 'infomation valid' do
       expect(page).to have_title(@user.name)
     end
   end
@@ -42,14 +48,46 @@ RSpec.describe "UsersLogins", type: :request do
       fill_in 'Email', with: @user.email
       fill_in 'Password', with: @user.password
 
-      click_button "Log in"
+      click_button 'Log in'
     end
     it 'followed by logout' do
       expect(page).to have_title(@user.name)
-      click_link "Log out"
 
-      expect(page).to have_title("Home")
+      click_link 'Log out'
+
+      expect(page).to have_title('Home')
+
+      delete logout_path
+
+      expect(page).to have_title('Home')
+    end
+  end
+
+  describe 'POST #create' do
+    let(:valid_parameters) do
+      {email: @user.email, password: @user.password}
     end
 
+    it 'saves the user ID to the session object' do
+      visit login_path
+
+      post login_path, session: valid_parameters
+
+      expect(session[:user_id]).to_not be nil
+    end
+  end
+
+  describe 'remembering' do
+    it 'log in with remembering' do
+      log_in_as(@user, remember_me: '1')
+
+      expect(cookies['remember_token']).to_not be nil
+    end
+
+    it 'login without remembering' do
+      log_in_as(@user, remember_me: '0')
+
+      expect(cookies['remember_token']).to be nil
+    end
   end
 end
