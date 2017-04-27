445a7ee5d63eaca3c21bad29fce43c45fd479197
swamp09 <shu.numata@esm.co.jp>
2017-04-27 13:07:14 +0900

    Implement basic login

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :michael, class: User do
+    name "Michael Example"
+    email "michael@example.com"
+    password "password"
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+require 'factory_girl_rails'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
@@ -26,10 +27,13 @@ include ApplicationHelper
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
-
+def is_logged_in?
+  !session[:user_id].nil?
+end
 # logger
 # Rails.logger = Logger.new(STDOUT) # 追記
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include Capybara::DSL
