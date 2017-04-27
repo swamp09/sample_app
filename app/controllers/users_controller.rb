e3f71ec7c193f7e32af69e5badcc307b0a4320c3
swamp09 <shu.numata@esm.co.jp>
2017-04-27 18:21:08 +0900

    :put_litter_in_its_place:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,15 +18,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
-  end
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                    :password_confirmation)
+    end
 end
