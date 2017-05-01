0eb8ae8b1ac8d78182ec41b0a46cbd3aecb40b20
swamp09 <shu.numata@esm.co.jp>
2017-05-01 11:48:52 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,15 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -36,11 +45,24 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
 
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
   def log_out
     forget(current_user)
 
     session.delete(:user_id)
 
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
---
+++ spec/requests/users_edits_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe "UsersEdits", type: :request do
+  before do
+    @user = FactoryGirl.create(:michael)
+  end
+  describe "GET /users_edits" do
+    let(:name) { "Foo bar" }
+    let(:email) { "foo@bar.com" }
+
+    before do
+      login_button(@user)
+      visit edit_user_path(@user)
+    end
+
+    describe 'unsuccessful edit' do
+      it do
+        expect(page).to have_title("Edit user")
+
+        patch user_path(@user), params:{user:{name:"",
+                          email:"foo@invalid",password:"foo",
+                          password_confirmation:"bar"}}
+
+        expect(page).to have_title("Edit user")
+      end
+    end
+
+    describe 'successfull edit' do
+      let(:name) { "Foo bar" }
+      let(:email) { "foo@bar.com" }
+
+      before do
+        fill_in "Name",             with: name
+        fill_in "Email",            with: email
+        fill_in "Password",         with: ""
+        fill_in "Password confirmation", with: ""
+
+        click_button "Save changes"
+      end
+
+      it do
+        expect(@user.reload.name).to eq(name)
+        expect(@user.reload.email).to eq(email)
+      end
+    end
+
+    describe "successful edit with friendly forwarding" do
+      subject {patch user_path(@user),params:{user:{email:name,
+                                  password:email}}}
+
+      before do
+        fill_in "Name",             with: name
+        fill_in "Email",            with: email
+        fill_in "Password",         with: ""
+        fill_in "Password confirmation", with: ""
+
+        click_button "Save changes"
+      end
+
+      it do
+        get edit_user_path(@user)
+
+        expect(flash).to_not be_nil
+
+        expect(subject).to redirect_to(login_url)
+
+        expect(@user.reload.name).to eq(name)
+        expect(@user.reload.email).to eq(email)
+      end
+
+
+    end
+  end
+end
