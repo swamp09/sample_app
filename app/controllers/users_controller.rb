871d9cb64ca00c1d4ab806dd15fd5b485cf68d1c
swamp09 <shu.numata@esm.co.jp>
2017-05-12 15:40:24 +0900

    Change from Spec Request to cucumber user_indices

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.where(activated: true).page(params[:page]).per(1)
+    @users = User.where(activated: true).page(params[:page])
   end
 
   def show
