23cf4c0797a9d58b5038718c2f32c603751be9f9
swamp09 <shu.numata@esm.co.jp>
2017-05-22 18:09:12 +0900

    Add RSpec agreegate_failures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,4 +25,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+
+  config.define_derived_metadata do |meta|
+    meta[:aggregate_failures] = true unless meta.key?(:aggregate_failures)
+  end
 end
