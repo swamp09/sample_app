4d046ec5951e26274d4d09e1189d10b184ae89fa
swamp09 <shu.numata@esm.co.jp>
2017-05-24 17:05:37 +0900

    Change from Spec request to feature micropost_interface

--- spec/requests/microposts_interface_test.rb
+++ spec/requests/microposts_interface_test.rb
@@ -3,25 +3,10 @@ require 'rails_helper'
 RSpec.describe 'MicropostsInterface', type: :request do
   before do
     @user = create(:michael)
-    @micropost = @user.microposts.create(attributes_for(:orange))
 
     login_button @user
 
     visit root_path
-  end
-
-  describe 'should redirect create when not logeed in' do
-    it 'should create a micropost' do
-      fill_in 'micropost_content', with: 'Lorem ipsum'
-
-      expect { click_button 'Post' }.to change(Micropost, :count).by(1)
-    end
-  end
-
-  describe 'should redirect destroy when not logeed in' do
-    it 'should delete a micropost' do
-      expect { click_link 'delete' }.to change(Micropost, :count).by(-1)
-    end
   end
 
   describe 'should not view for wrong micropost' do
