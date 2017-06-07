d3076c54b11730f07b3d13b51d181c451819dbaa
swamp09 <shu.numata@esm.co.jp>
2017-06-07 16:38:53 +0900

    Cosmetic Change

--- spec/steps/user_login_steps.rb
+++ spec/steps/user_login_steps.rb
@@ -1,5 +1,3 @@
-# encoding: utf-8
-
 step 'ユーザーがアカウント登録済みである' do
   @user = create(:michael)
 
@@ -10,24 +8,22 @@ step 'ログインページにアクセスする' do
   visit login_path
 end
 
-step 'ログイン画面が表示されている' do
-  expect(page).to have_title('Log in | Ruby on Rails Tutorial Sample App')
+step ':title画面が表示されている' do |title|
+  expect(page).to have_title(title)
 end
 
-step 'email と password を入力する' do
-  fill_in 'Email', with: @user.email
-  fill_in 'Password', with: @user.password
+step ':email と :password を入力する' do |email, password|
+  fill_in email, with: @user.email
+  fill_in password, with: @user.password
 end
 
-step 'ログインボタンをクリックする' do
-  click_button 'Log in'
+step ':textボタンをクリックする' do |text|
+  click_button text
 end
 
 step 'ログイン後のユーザー画面を表示する' do
   expect(page).to have_title(@user.name)
 end
-
-#####################
 
 step 'ユーザーがアカウント登録済みである' do
   @user = create(:michael)
@@ -42,12 +38,12 @@ end
 step 'ユーザーがフォームに入力しない' do
 end
 
-step 'ログインボタンをクリックする' do
-  click_button 'Log in'
+step ':textボタンをクリックする' do |text|
+  click_button text
 end
 
-step 'ログイン画面にとどまり、アラートを表示し入力を促す' do
-  expect(page).to have_title('Log in | Ruby on Rails Tutorial Sample App')
+step ':title画面にとどまり、アラートを表示し入力を促す' do |title|
+  expect(page).to have_title(title)
 
   expect(page).to have_selector('div.alert.alert-danger', text: 'Invalid')
 end
