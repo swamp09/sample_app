0acb1e845b56c1cb60f412bac896c67e555c01b5
swamp09 <shu.numata@esm.co.jp>
2017-06-08 12:11:33 +0900

    Add micropost search

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
     return unless logged_in?
 
     @micropost  = current_user.microposts.build
-    @feed_items = current_user.feed.page(params[:page])
+    @feed_items = current_user.feed(params[:search]).page(params[:page])
   end
 
   def help; end
